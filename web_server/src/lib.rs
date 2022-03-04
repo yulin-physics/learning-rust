52761139ed5eb6d756ea04892fde7819efabaf82
yulin-physics <yulin.physics@gmail.com>
2022-03-04 11:02:01 +0000

    executes connections asynchronously

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,8 +1,14 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -15,19 +21,25 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+        // to share ownership across multiple threads and allow the threads to mutate the value, we need to use Arc<Mutex<T>>. The Arc type will let multiple workers own the receiver, and Mutex will ensure that only one worker gets a job from the receiver at a time.
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -37,8 +49,14 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // we need the closure to loop forever, asking the receiving end of the channel for a job and running the job when it gets one.
+        let thread = thread::spawn(move || loop {
+            // let, any temporary values used in the expression on the right hand side of the equals sign are immediately dropped when the let statement ends
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {} get a job; executing.", id);
+            job()
+        });
         Worker { id, thread }
     }
 }
