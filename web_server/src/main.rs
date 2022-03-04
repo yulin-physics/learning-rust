190d70faf4a3f55b8029766adae9bd422b1f7317
yulin-physics <yulin.physics@gmail.com>
2022-03-04 09:25:43 +0000

    simulate slow request in single thread

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -3,7 +3,8 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-
+use std::thread;
+use std::time::Duration;
 fn main() {
     // Listen to TCP connection
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -24,11 +25,12 @@ fn handle_connection_with_validation(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     let (status_line, filename) = if buffer.starts_with(get) {
-        (
-            "HTTP/1.1 200 OK\r\nContent-Leng: {}\r\n\r\n{}",
-            "hello.html",
-        )
+        ("HTTP/1.1 200 OK", "home.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "home.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
