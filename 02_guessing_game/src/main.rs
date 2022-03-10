0fe467dd2a686fdf3526abb8590bfc9ea6651b22
yulin-physics <yulin.physics@gmail.com>
2022-03-10 15:38:20 +0000

    chapter 9 error handling

--- 02_guessing_game/src/main.rs
+++ 02_guessing_game/src/main.rs
@@ -2,10 +2,27 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        };
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number");
 
-    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+    let secret_number: i32 = rand::thread_rng().gen_range(1..101);
     println!("secret number: {}", secret_number);
 
     loop {
@@ -21,10 +38,13 @@ fn main() {
 
         // reuse guess variable: shadow the previous value of guess with a new one.
         // let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        let guess = Guess::new(guess).value();
+
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
---
+++ 09_error_handling/src/main.rs
@@ -0,0 +1,73 @@
+// Result<T, E> for recoverable errors
+// panic! macro for unrecoverable error, stops execution
+
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // use match expression
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            // create file if doesn't exist
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(error) => panic!("problem creating teh file: {:?}", error),
+            },
+            // panic for other error kinds
+            other_error => {
+                panic!("problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    // use closure
+    let f = File::open("hello.txt").unwrap_or_else(|e| {
+        if e.kind() == ErrorKind::NotFound {
+            File::create("hello.txt")
+                .unwrap_or_else(|e| panic!("problem creating the file: {:?}", e))
+        } else {
+            panic!("problem opening the file: {:?}", e)
+        }
+    });
+
+    // shortcut for panic on error
+    let f = File::open("hello.txt").unwrap();
+
+    // choose panic error message with expect
+    let f = File::open("hi.txt").expect("file opening error");
+
+    Ok(())
+}
+
+// propagating errors
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        // read to string returns a usize, so use a underscore _ in ok to omit the usize
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// propagating errors shorcut
+// ? operator: Err will be returned from the whole function as if we had used the return keyword
+fn read_username() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
