64380e7beffc36e45593cb9e40ccc46872921e74
yulin-physics <yulin.physics@gmail.com>
2022-04-28 12:29:56 +0100

    test function

---
+++ benchmarking/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn swap(numbers: &mut Vec<i32>, i: usize, j: usize) {
+    let temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+}
+
+// sort in place by passing mutable reference
+pub fn insertion_sorter(numbers: &mut Vec<i32>) {
+    for i in 1..numbers.len(){
+        let mut j=i;
+        while j>0 && numbers[j-1]> numbers[j]{
+            swap(numbers, j, j-1);
+            j = j-1;
+        }
+    }
+}
